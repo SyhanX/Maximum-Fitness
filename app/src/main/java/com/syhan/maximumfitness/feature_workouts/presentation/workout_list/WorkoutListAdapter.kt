2a8c169996232528e597b0cd9eef1bdf27949436
syhan <oboznyy.maks99@gmail.com>
2025-05-19 02:09:36 +0700

    make workout_card.xml look prettier

--- app/src/main/java/com/syhan/maximumfitness/feature_workouts/presentation/workout_list/WorkoutListAdapter.kt
+++ app/src/main/java/com/syhan/maximumfitness/feature_workouts/presentation/workout_list/WorkoutListAdapter.kt
@@ -1,11 +1,15 @@
 package com.syhan.maximumfitness.feature_workouts.presentation.workout_list
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.syhan.maximumfitness.R
 import com.syhan.maximumfitness.databinding.WorkoutCardBinding
+import com.syhan.maximumfitness.feature_workouts.data.WorkoutType
+import com.syhan.maximumfitness.feature_workouts.data.getMinutesDeclension
 import com.syhan.maximumfitness.feature_workouts.presentation.workout_list.state.WorkoutCardState
 
 class WorkoutListAdapter : ListAdapter<WorkoutCardState, WorkoutListAdapter.WorkoutStateViewHolder>(
@@ -18,6 +22,27 @@ class WorkoutListAdapter : ListAdapter<WorkoutCardState, WorkoutListAdapter.Work
         fun bind(item: WorkoutCardState) {
             binding.apply {
                 title.text = item.title
+
+                type.text = root.resources.getString(
+                    WorkoutType.entries[item.type -1].typeName
+                )
+
+                description.text = item.description
+                    ?: root.resources.getString(R.string.no_description)
+
+                durationRow.visibility = if (item.duration == -1) View.GONE else View.VISIBLE
+
+                duration.text = root.resources.getString(
+                    getMinutesDeclension(item.duration),
+                    item.duration
+                )
+
+                expandDescriptionButton.setOnClickListener {
+
+                }
+                root.setOnClickListener {
+
+                }
             }
         }
     }
