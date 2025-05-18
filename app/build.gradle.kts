e50306dd515cca8c083590c611d0d93981ba322a
syhan <oboznyy.maks99@gmail.com>
2025-05-19 02:34:29 +0700

    up minSdk number to prevent suffering with compatibility issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.syhan.maximumfitness"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
