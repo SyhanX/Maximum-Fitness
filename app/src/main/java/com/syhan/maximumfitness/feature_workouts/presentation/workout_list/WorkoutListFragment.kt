0ff7963afd85ae8ae4bf79adfb7be2bfe1d5627b
syhan <oboznyy.maks99@gmail.com>
2025-05-18 16:54:21 +0700

    implement basic workout list

--- app/src/main/java/com/syhan/maximumfitness/feature_workouts/presentation/workout_list/WorkoutListFragment.kt
+++ app/src/main/java/com/syhan/maximumfitness/feature_workouts/presentation/workout_list/WorkoutListFragment.kt
@@ -1,34 +1,75 @@
 package com.syhan.maximumfitness.feature_workouts.presentation.workout_list
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.syhan.maximumfitness.R
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.syhan.maximumfitness.common.domain.NetworkRequestUiState
 import com.syhan.maximumfitness.databinding.FragmentWorkoutListBinding
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
+private const val TAG = "WorkoutListFragment"
 class WorkoutListFragment : Fragment() {
 
     private var _binding: FragmentWorkoutListBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: WorkoutListViewModel by viewModel()
+
+    private lateinit var recyclerView: RecyclerView
+    private val workoutListAdapter = WorkoutListAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentWorkoutListBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        recyclerView = binding.workoutRecyclerView
+        setupRecyclerView()
+        submitDataToRecyclerView()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.networkUiState.collect { uiState ->
+                when (uiState) {
+                    NetworkRequestUiState.Loading -> {
+                        Log.d(TAG, "onViewCreated: loading")
+                    }
+                    is NetworkRequestUiState.Error -> {
+                        Log.d(TAG, "onViewCreated: error ${uiState.type}")
+                    }
+                    NetworkRequestUiState.Success -> {
+                        Log.d(TAG, "onViewCreated: success")
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun setupRecyclerView() {
+        recyclerView.apply {
+            adapter = workoutListAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun submitDataToRecyclerView() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.listState.collect { state ->
+                workoutListAdapter.submitList(state.list)
+            }
         }
     }
 
