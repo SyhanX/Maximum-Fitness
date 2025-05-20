0e16214c38611bd28be7f966cf6758c1ca0018b3
syhan <oboznyy.maks99@gmail.com>
2025-05-20 15:24:06 +0700

    add view visibility extensions

---
+++ app/src/main/java/com/syhan/maximumfitness/common/data/SetViewVisibility.kt
@@ -0,0 +1,15 @@
+package com.syhan.maximumfitness.common.data
+
+import android.view.View
+
+fun View.setVisible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.setInvisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.setGone() {
+    this.visibility = View.GONE
+}
