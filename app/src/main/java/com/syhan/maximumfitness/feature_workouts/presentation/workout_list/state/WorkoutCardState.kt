dd924d5e020112d8436095ce50ce149e7ca3f5a9
syhan <oboznyy.maks99@gmail.com>
2025-05-24 17:34:48 +0700

    change default values, remove redundant onClick property

--- app/src/main/java/com/syhan/maximumfitness/feature_workouts/presentation/workout_list/state/WorkoutCardState.kt
+++ app/src/main/java/com/syhan/maximumfitness/feature_workouts/presentation/workout_list/state/WorkoutCardState.kt
@@ -1,10 +1,12 @@
 package com.syhan.maximumfitness.feature_workouts.presentation.workout_list.state
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class WorkoutCardState(
     val id: Int = -1,
     val title: String = "",
     val description: String? = null,
-    val type: Int = 0,
-    val duration: Int = -1,
-    val onClick: (id: Int) -> Unit = {},
+    val type: Int = 1,
+    val duration: Int = 0,
 )
