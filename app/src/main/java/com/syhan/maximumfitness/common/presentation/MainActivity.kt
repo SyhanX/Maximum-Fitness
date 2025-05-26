985128dd66bd531f17df2bf15c2c53133c630440
syhan <oboznyy.maks99@gmail.com>
2025-05-26 13:27:33 +0700

    refactor code

--- app/src/main/java/com/syhan/maximumfitness/common/presentation/MainActivity.kt
+++ app/src/main/java/com/syhan/maximumfitness/common/presentation/MainActivity.kt
@@ -2,14 +2,10 @@ package com.syhan.maximumfitness.common.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import com.syhan.maximumfitness.R
 import com.syhan.maximumfitness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +14,5 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
-        val navController = navHostFragment.navController
     }
 }
