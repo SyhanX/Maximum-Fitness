de66dd5a08202851d29487254dd60b40fc03da28
syhan <oboznyy.maks99@gmail.com>
2025-05-26 13:27:23 +0700

    fix bugs

--- app/src/main/java/com/syhan/maximumfitness/feature_workouts/presentation/workout_list/WorkoutListFragment.kt
+++ app/src/main/java/com/syhan/maximumfitness/feature_workouts/presentation/workout_list/WorkoutListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -142,5 +143,12 @@ class WorkoutListFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val types = resources.getStringArray(R.array.dropdown_items)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, types)
+        binding.sortingMenu.setAdapter(arrayAdapter)
     }
 }
